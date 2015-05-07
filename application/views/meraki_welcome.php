3c3c5078c01f7d2a4179a9cba048aff13be8ca07
Winzter Corpuz <lover_winzter143@yahoo.com>
2015-05-07 16:30:16 +0800

    [UPDATE] - Add the continue link

--- application/views/meraki_welcome.php
+++ application/views/meraki_welcome.php
@@ -1,1 +1,3 @@
-<h1>Welcome <?php echo $name;?></h1>
+<h1>Welcome <?php echo $name;?></h1> 
+
+<a href="<?php echo $base_grant_url.'?continue='.$user_continue_url.'&duration=120'?>">Click to continue.......</a>
